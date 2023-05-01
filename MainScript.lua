11fda12d210cd02f5e0c38ee3b8a8965c8b63e65
BaconKidDumb <130199986+BaconKidDumb@users.noreply.github.com>
2023-05-01 01:20:27 -0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -13,7 +13,7 @@ else
 	shared.AshuraExecuted = true
 end
 
-local GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/AshuraForRoblox/main/GuiLibrary.lua", true))()
+local GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/BaconKidDumb/AshuraForRoblox/main/GuiLibrary.lua", true))()
 shared.GuiLibrary = GuiLibrary
 
 if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
